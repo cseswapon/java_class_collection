ec7249bd03e44a51652276b44981f92b6199c313
Swapon Saha <cseswaponsaha@gmail.com>
2025-06-20 23:36:56 +0600

    Update LList.java

--- LList/src/main/java/com/mycompany/llist/LList.java
+++ LList/src/main/java/com/mycompany/llist/LList.java
@@ -1,10 +1,7 @@
 package com.mycompany.llist;
 import java.util.LinkedList;
 import java.util.List;
-/**
- *
- * @author swapon
- */
+
 public class LList {
 
     public static void main(String[] args) {
